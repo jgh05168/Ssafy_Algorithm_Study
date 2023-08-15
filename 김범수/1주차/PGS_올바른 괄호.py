74f8145c34843435b0c708b433676bfccd1b1916
markrla <markrla@naver.com>
2023-08-15 16:53:33 +0900

    123

--- 김범수/1주차/PGS_올바른 괄호.py
+++ 김범수/1주차/PGS_올바른 괄호.py
@@ -4,9 +4,11 @@ def solution(s):
     for i in s:
         if i == '(':
             stack.append(i)
+            print(stack)
         else:
             if len(stack) > 0 and stack[-1] =='(':
                 stack.pop()
+                print(stack)
             else:
                 return False
     else:
@@ -14,4 +16,8 @@ def solution(s):
             return True
         else:
             return False
+
+print(solution('(())()'))
+print()
+print(solution('(()()'))
 
